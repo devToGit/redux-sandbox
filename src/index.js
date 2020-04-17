0e55aa0c356709b72b9163410c46aff1926a612a
devToGit <nnos@mail.ru>
2020-04-17 12:43:47 +0300

    added React & Redux in App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
+
+import Counter from './counter';
+
 import reducer from './reducer';
 import * as actions from './actions';
 
@@ -9,25 +14,19 @@ const { dispatch } = store;
 const { inc, dec, rnd} = 
   bindActionCreators(actions, dispatch);
 
-document
-  .getElementById('inc')
-  .addEventListener('click', inc);
-
-document
-  .getElementById('dec')
-  .addEventListener('click', dec);
-
-document
-  .getElementById('rnd')
-  .addEventListener('click', () => {
-    const payLoad = Math.floor(Math.random()*10);
-      rnd(payLoad);
-  });
-
 const update = () => {
-  document
-    .getElementById('counter')
-    .innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter 
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random()*10);
+        rnd(value);
+      }}/>, 
+    document.getElementById('root'));
 };
 
+update();
 store.subscribe(update);
+
